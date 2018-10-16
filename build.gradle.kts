a19e172602f4b655dd51a197bbfabf6e61a55098
Ross Lawley <ross.lawley@gmail.com>
2018-10-16 17:29:37 -0400

    Stubbed the Structure Standard test

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,23 @@ plugins {
     kotlin("jvm") version "1.2.71"
 }
 
+val kotlinVersion: String by project
+val tinkerpopVersion: String by project
+val kmongoVersion: String by project
+val junitVersion: String by project
+
 application {
     mainClassName = "samples.HelloWorldKt"
 }
 
 dependencies {
     api(kotlin("stdlib"))
-    api("org.litote.kmongo:kmongo:3.8.3")
-    api("org.apache.tinkerpop:tinkergraph-gremlin:3.3.3")
+    api("org.litote.kmongo:kmongo:$kmongoVersion")
+    api("org.apache.tinkerpop:tinkergraph-gremlin:$tinkerpopVersion")
+
+    testApi("org.apache.tinkerpop:gremlin-test:$tinkerpopVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("junit:junit:$junitVersion")
 }
 
 repositories {

457ee67498128944df44724cd98c894d112215ee
alisovenko <alisovenko@gmail.com>
2018-10-18 09:34:26 -0400

    added MongoVertexProperty and TinkerProperty to MongoPlugin

--- src/main/java/com/mongodb/mongopop/gremlin/MongoPlugin.java
+++ src/main/java/com/mongodb/mongopop/gremlin/MongoPlugin.java
@@ -14,46 +14,49 @@
  * limitations under the License.
  */
 
-
 package com.mongodb.mongopop.gremlin;
 
 import org.apache.tinkerpop.gremlin.jsr223.AbstractGremlinPlugin;
 import org.apache.tinkerpop.gremlin.jsr223.DefaultImportCustomizer;
 import org.apache.tinkerpop.gremlin.jsr223.ImportCustomizer;
 import com.mongodb.mongopop.gremlin.structure.*;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerProperty;
+
 
 @SuppressWarnings("ALL")
 public final class MongoPlugin extends AbstractGremlinPlugin {
 
-    private static final String NAME = "tinkerpop.mongo";
+  private static final String NAME = "tinkerpop.mongo";
 
-    private static final ImportCustomizer imports;
+  private static final ImportCustomizer imports;
 
-    static {
-        try {
-            imports = DefaultImportCustomizer.build()
-                    .addClassImports(MongoEdge.class,
-                            MongoElement.class,
-                            MongoGraph.class,
-                            MongoProperty.class,
-                            MongoVertex.class).create();
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
+  static {
+    try {
+      imports = DefaultImportCustomizer.build()
+        .addClassImports(MongoEdge.class,
+          MongoElement.class,
+          MongoGraph.class,
+          MongoProperty.class,
+          TinkerProperty.class,
+          MongoVertexProperty.class,
+          MongoVertex.class).create();
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
     }
+  }
 
-    private static final MongoPlugin instance = new MongoPlugin();
+  private static final MongoPlugin instance = new MongoPlugin();
 
-    public MongoPlugin() {
-        super(NAME, imports);
-    }
+  public MongoPlugin() {
+    super(NAME, imports);
+  }
 
-    public static MongoPlugin instance() {
-        return instance;
-    }
+  public static MongoPlugin instance() {
+    return instance;
+  }
 
-    @Override
-    public boolean requireRestart() {
-        return true;
-    }
+  @Override
+  public boolean requireRestart() {
+    return true;
+  }
 }
